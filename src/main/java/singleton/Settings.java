9238dd3c8bf87d7eefc1f913d0a26ddc4d3da38f
suho0213 <ai_rnaster@kakao.com>
2022-04-03 00:57:34 +0900

    디자인 패턴 # singleton 02 / eager initialization 방법 사용

--- src/main/java/singleton/Settings.java
+++ src/main/java/singleton/Settings.java
@@ -2,15 +2,12 @@ package main.java.singleton;
 
 public class Settings {
 
-    private static Settings instance;
+    private static final Settings INSTANCE = new Settings();
 
     private  Settings() {}
 
-    public static synchronized Settings getInstance(){
-        if (instance == null) {
-            instance = new Settings();
-        }
-        return instance;
+    public static Settings getInstance(){
+        return INSTANCE;
     }
 
 
