00645aa51841d9993bdd018caf021acfe4cd483d
suho0213 <ai_rnaster@kakao.com>
2022-04-03 11:23:12 +0900

    디자인 패턴 # singleton 03 / 리플렉션 사용

--- src/main/java/singleton/App.java
+++ src/main/java/singleton/App.java
@@ -1,9 +1,16 @@
 package main.java.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings = Settings.getInstance();
 
-        System.out.println(settings == Settings.getInstance());
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings1 = constructor.newInstance();
+
+        System.out.println(settings == settings1);
     }
 }
